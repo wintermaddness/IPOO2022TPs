9aa371aa6bd59c84f99fc3a22d527f96c83ce8f8
Celeste Aliaga <celeste.aliaga@est.fi.uncoma.edu.ar>
2022-04-05 12:37:55 -0300

    Subida de archivos

---
+++ Test_ViajeCelesteAliaga.php
@@ -0,0 +1,86 @@
+<?php
+    include "ViajeCelesteAliaga.php";
+	echo ">>> Por favor, ingrese los datos del viaje <<<\n";
+	echo "| Ingrese el código de viaje: ";
+	$codViaje = trim(fgets(STDIN));
+	echo "| Ingrese el destino: ";
+	$destino = trim(fgets(STDIN));
+	echo "| Ingrese la capacidad máxima de pasajeros: ";
+	$capacidadMaxima = trim(fgets(STDIN));
+	$arrayPasajeros = [];
+
+	$objViaje = new Viaje($codViaje, $destino, $capacidadMaxima, $arrayPasajeros);
+
+	do {
+		$menu = menuDeOpciones();
+		echo $menu;
+		$opcion = trim(fgets(STDIN));
+		switch ($opcion) {
+			case 1:
+				echo  "\n--------- Cargar pasajero ---------\n" ;
+				echo  "| ¿Cuántos pasajeros desea ingresar?: ";
+				$limitePasajeros = trim(fgets(STDIN));
+				for ($i=0; $i<$limitePasajeros; $i++) {
+					echo "\n	+ Ingrese el nombre del pasajero: ";
+					$arregloPasajeros[$i]["nombre"] = trim(fgets(STDIN));
+					echo "	+ Ingrese el apellido del pasajero: ";
+					$arregloPasajeros[$i]["apellido"] = trim(fgets(STDIN));
+					echo "	+ Ingrese el número de documento del pasajero: ";
+					$arregloPasajeros[$i]["dni"] = trim(fgets(STDIN));
+				}
+				if ($i == 0){
+					echo "\n>>> No ingresó ningún pasajero.\n";
+				} else {
+					$objViaje->agregarPasajeros($arregloPasajeros);
+					echo  "\n>>> Se ingresó/ingresaron ".$i." pasajero/s con éxito.\n";
+				}
+				break;
+			case 2:
+				echo "----- Modificar pasajero -----\n";
+				echo "| Ingrese el número del pasajero a modificar: ";
+				$indPasajero = trim(fgets(STDIN));
+				echo "\n	+ Ingrese el nuevo nombre del pasajero: ";
+				$nuevoNombre = trim(fgets(STDIN));
+				echo "	+ Ingrese el nuevo apellido del pasajero: ";
+				$nuevoApellido = trim(fgets(STDIN));
+				echo "	+ Ingrese el nuevo número de documento del pasajero: ";
+				$nuevoDni = trim(fgets(STDIN));
+				$objViaje->modificarPasajeros($indPasajero, $nuevoNombre, $nuevoApellido, $nuevoDni);
+				echo  "\n>>> Pasajero modificado exitosamente.\n";
+				break;
+			case 3:
+				echo "----- Eliminar pasajero -----\n" ;
+				echo "| Ingrese el número del pasajero que desea eliminar: ";
+				$eliminarPasajero = trim(fgets(STDIN));
+				$objViaje->eliminarPasajeros($eliminarPasajero);
+				echo  "\n>>> Pasajero eliminado exitosamente.\n";
+				break;
+			case 4:
+				echo "----- Ver datos del viaje -----";
+				$cadena = $objViaje->__toString();
+    			echo $cadena."\n";
+				break;
+			case 5:
+				echo "----- Mostrar pasajero -----\n" ;
+				echo "| Ingrese el número del pasajero que desea ver: ";
+				$verPasajero = trim(fgets(STDIN));
+				$datos = $objViaje->mostrarPasajeros($verPasajero);
+				echo $datos;
+				break;
+			case 6:
+				echo "\n>>> Ha salido del programa.";
+				break;
+		}
+	} while($opcion != 6);
+
+	function menuDeOpciones() {
+		$menu = "\n---------------Menú de opciones---------------".
+		"\n| Opción 1: Agregar pasajeros".
+		"\n| Opción 2: Modificar pasajeros".
+		"\n| Opción 3: Eliminar pasajeros".
+		"\n| Opción 4: Ver datos del viaje".
+		"\n| Opción 5: Ver datos del pasajero".
+		"\n| Opción 6: Salir del programa\n";
+		return $menu;
+	}
+?>
---
+++ ViajeCelesteAliaga.php
@@ -0,0 +1,127 @@
+<?php
+    class Viaje {
+        private $codigoViaje;
+        private $destino;
+        private $capacidadPasajeros;
+        private $pasajeros;
+
+        //Métodos de acceso
+        public function getCodigoViaje() {
+            return $this->codigoViaje;
+        }
+        public function getDestino() {
+            return $this->destino;
+        }
+        public function getCapacidadPasajeros() {
+            return $this->capacidadPasajeros;
+        }
+        public function getPasajeros() {
+            return $this->pasajeros;
+        }
+
+        public function setCodigoViaje($codigoViaje) {
+            $this->codigoViaje = $codigoViaje;
+        }
+        public function setDestino($destino) {
+            $this->destino = $destino;
+        }
+        public function setCapacidadPasajeros($capacidadPasajeros) {
+            $this->capacidadPasajeros = $capacidadPasajeros;
+        }
+        public function setPasajeros($pasajeros) {
+            $this->pasajeros = $pasajeros;
+        }
+        
+        //Métodos
+        public function __construct($codigoViaje, $destino, $capacidadPasajeros, $pasajeros) {
+            $this->codigoViaje = $codigoViaje;
+            $this->destino = $destino;
+            $this->capacidadPasajeros = $capacidadPasajeros;
+            $this->pasajeros = $pasajeros;
+        }
+
+        /**
+         * Método 1: datosViaje - 
+         * Retorna un arreglo con los datos de los viajes.
+         * @return array
+         */
+        public function datosViaje($codigoViaje, $destino, $capacidadPasajeros) {
+            $this->setCodigoViaje($codigoViaje);
+            $this->setDestino($destino);
+            $this->setCapacidadPasajeros($capacidadPasajeros);
+        }
+
+        /**
+         * Método 2: agregarPasajeros - 
+         * Retorna un arreglo con los datos de los pasajeros agregados (agrega pasajeros).
+         * @return array
+         */
+        public function agregarPasajeros($nuevoPasajero) {
+            $arrayPasajeros = $this->getPasajeros();
+            $indMaximo = count($nuevoPasajero) - 1;
+            for ($i=0; $i<=$indMaximo; $i++) {
+                array_push($arrayPasajeros, $nuevoPasajero[$i]);
+            }        
+            $this->setPasajeros($arrayPasajeros);
+        }
+
+        /**
+         * Método 3: eliminarPasajeros - 
+         * Elimina a un pasajero determinado.
+         */
+        public function eliminarPasajeros($indicePasajero) {
+            $indice = $indicePasajero - 1;
+            $arrayPasajeros = $this->getPasajeros();
+            array_splice($arrayPasajeros, $indice, 1);
+            $this->setPasajeros($arrayPasajeros);
+        }
+
+        /**
+         * Método 4: modificarPasajeros - 
+         * Modifica los datos de un pasajero.
+         * NOTA: Se puede usar "*" para dejar algún dato igual/sin modificar.
+         */
+        public function modificarPasajeros($indicePasajero, $nombre, $apellido, $dni) {
+            $indice = $indicePasajero - 1;
+            $arrayPasajeros = $this->getPasajeros();
+            if ($nombre != "*") {
+                $arrayPasajeros[$indice]["nombre"] = $nombre;
+            }
+            if ($apellido != "*") {
+                $arrayPasajeros[$indice]["apellido"] = $apellido;
+            }
+            if ($dni != "*") {
+                $arrayPasajeros[$indice]["dni"] = $dni;
+            }
+            $this->setPasajeros($arrayPasajeros);
+        }
+
+        /**
+         * Método 5: mostrarPasajeros - 
+         * Muestra los datos de un pasajero.
+         * @return string
+         */
+        public function mostrarPasajeros($indicePasajero) {
+            $arrayPasajeros = $this->getPasajeros();
+            if ($indicePasajero > (count($arrayPasajeros) - 1)) {
+                $cadena = "\n>>> ERROR. El número ingresado no tiene un pasajero asignado.\n";
+            } else {
+                for ($i=0; $i<(count($arrayPasajeros) - 1); $i++) {
+                $cadena = "\n| Pasajero N°: ".($indicePasajero).
+                            "\n| Nombre: ".$arrayPasajeros[$indicePasajero]["nombre"].
+                            "\n| Apellido: ".$arrayPasajeros[$indicePasajero]["apellido"].
+                            "\n| Documento: ".$arrayPasajeros[$indicePasajero]["dni"]."\n";
+                }
+            }
+            return $cadena;
+        }
+
+        public function __toString() {
+            $cadena = "\n| Codigo de viaje: ".$this->getCodigoViaje()."\n"
+                        ."| Destino: ".$this->getDestino()."\n"
+                        ."| Capacidad de pasajeros: ".$this->getCapacidadPasajeros()."\n"
+                        ."| Cantidad de pasajeros: " .count($this->getPasajeros())."\n"; 
+            return $cadena;
+        }
+    }
+?>
